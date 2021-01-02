31c35c33edcf8160d5016041c5aad7d35052c486
Youngseok <jiys5164@naver.com>
2021-01-02 18:01:38 +0900

    Promise가 원하는 대로 작동하지 않는 문제 수정

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,23 +6,25 @@ import {
 
 export default {
   FETCH_LIST({ commit }, pageName) {
-    fetchList(pageName).then(({ data }) => {
-      commit('SET_LIST', data)
-      return data
+    return fetchList(pageName).then(response => {
+      commit('SET_LIST', response.data)
+      return response
     }).catch(error => {
       console.log(error)
     })
   },
   FETCH_USER({ commit }, userName) {
-    fetchUserInfo(userName).then(({ data }) => {
-      commit('SET_USER', data)
+    return fetchUserInfo(userName).then(response => {
+      commit('SET_USER', response.data)
+      return response
     }).catch(error => {
       console.log(error)
     })
   },
   FETCH_ITEM({ commit }, itemId) {
-    fetchCommentItem(itemId).then(({ data }) => {
-      commit('SET_ITEM', data)
+    return fetchCommentItem(itemId).then(response => {
+      commit('SET_ITEM', response.data)
+      return response
     }).catch(error => {
       console.log(error)
     })
