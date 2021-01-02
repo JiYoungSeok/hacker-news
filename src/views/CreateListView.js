cea961de0f7f18d53b7fdcf3dcc137609d1c2e90
Youngseok <jiys5164@naver.com>
2021-01-02 20:50:01 +0900

    리스트 가져오는 API 호출 시점 변경 (Router Navigation Guard)

--- src/views/CreateListView.js
+++ src/views/CreateListView.js
@@ -1,17 +1,8 @@
 import ListView from '@/views/ListView.vue'
-import bus from '@/utils/bus.js'
 
 export default function createListView(name) {
   return {
     name,
-    created() {
-      bus.$emit('start:spinner')
-      this.$store.dispatch('FETCH_LIST', this.$route.name).then(() => {
-        bus.$emit('end:spinner')
-      }).catch(error => {
-        console.log(error)
-      })
-    },
     render(createElement) {
       return createElement(ListView)
     }
